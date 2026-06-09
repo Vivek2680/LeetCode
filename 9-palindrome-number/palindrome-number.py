aa2b095e0fef8df667143b7e1d281adcc32195e3
Vivek Kushwaha <rewaabhilash4@gmail.com>
2026-06-10 03:44:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.56%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        orignal = x# save orignal value of x cause x will change in each iteration of while loop and use it to compare
-        reversed_number = 0
-        while x > 0:
-            digit = x % 10 # extract the last digit
-            reversed_number = reversed_number*10 + digit # to build reverse order
-            x = x // 10 # to remove the last digit
-        return orignal == reversed_number # return compare result
+        num = str(x)
+
+        return num == num[ : :-1]
 obj = Solution()
-obj.isPalindrome(121)
+print(obj.isPalindrome(212))
