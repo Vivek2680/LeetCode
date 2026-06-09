fe3602bc07dbfd26823de9eab26d4dbea9fcbd32
Vivek Kushwaha <rewaabhilash4@gmail.com>
2026-06-10 02:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (42.44%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,5 +1,18 @@
+'''
+Finding the sum of a pair that is euqal to target sum 
+'''
+from typing import List
+'''
+imports list from python lib 
+for type hints
+'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        return and store in dict i.e. seen
+        it return the two number that sum up to target
+        if not then store it in seen dict
+        '''
         seen ={}
         for i , num in enumerate(nums):
             complement = target - num
@@ -7,10 +20,8 @@ class Solution:
                 return [seen[complement],i]
             seen[num] = i
         return []
+        '''
+        return empty list for the case where no pair found that there sun equal to target
+        '''
 obj = Solution()
-print(obj.twoSum([0,1,2,3,4,5,6,7,8,9],10))
-    
-        
-
-
-        
+print(obj.twoSum(nums=[0,1,2,3,4,5,6,7,8,9],target=10))
