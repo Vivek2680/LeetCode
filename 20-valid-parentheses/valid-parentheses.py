989da6d8e8684e40126a2b11b1326f43789a04e7
Vivek Kushwaha <rewaabhilash4@gmail.com>
2026-06-15 00:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -7,16 +7,23 @@ class Solution:
         }
         my_list = []
         for ch in s:
-            if  ch in parentheses_dict.values():
-                my_list.append(ch)
-            elif ch in parentheses_dict:
-                if not my_list:
+            # if  ch in parentheses_dict.values():
+            #     my_list.append(ch)
+            # elif ch in parentheses_dict:
+            #     if not my_list:
+            #         return False
+            #     else:
+            #         if my_list[-1] == parentheses_dict[ch]:
+            #             my_list.pop()
+            #         else:
+            #             return False
+            if ch in parentheses_dict:
+                if not my_list or my_list[-1] != parentheses_dict[ch]:
                     return False
-                else:
-                    if my_list[-1] == parentheses_dict[ch]:
-                        my_list.pop()
-                    else:
-                        return False
+                my_list.pop()
+            else:
+                my_list.append(ch)
+            
         return not my_list
         
 obj = Solution()
