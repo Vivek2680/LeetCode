eee0f8d0eb718b1ff7bde558e8f9360e0480c9bd
Vivek Kushwaha <rewaabhilash4@gmail.com>
2026-06-15 00:08:20 +0530

    Time: 3 ms (34.28%) | Memory: 19.3 MB (27.81%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses_dict = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        }
+        my_list = []
+        for ch in s:
+            if  ch in parentheses_dict.values():
+                my_list.append(ch)
+            elif ch in parentheses_dict:
+                if not my_list:
+                    return False
+                else:
+                    if my_list[-1] == parentheses_dict[ch]:
+                        my_list.pop()
+                    else:
+                        return False
+        return not my_list
+        
+obj = Solution()
+print(obj.isValid('()'))
